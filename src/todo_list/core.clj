ee637e19363c67f8341443813b5738092f8822a5
Aloisia Davì <aloisia.davi@gmail.com>
2019-05-17 18:49:56 +0200

    first commit

---
+++ src/todo_list/core.clj
@@ -0,0 +1,79 @@
+(ns todo-list.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
+
+(defn welcome
+  "A ring handler to respond with a simple welcome message"
+  [request]
+  {:status 200
+   :body "<h1>Hello, Clojure World</h1>
+     <p>Welcome to your first Clojure app, I now update automatically</p>
+     <p>I now use defroutes to manage incoming requests</p>"
+   :headers {}})
+
+(defn goodbye
+  "A song to wish you goodbye"
+  [request]
+  {:status 200
+   :body "<h1>Walking back to happiness</h1>
+          <p>Walking back to happiness with you</p>
+          <p>Said, Farewell to loneliness I knew</p>
+          <p>Laid aside foolish pride</p>
+          <p>Learnt the truth from tears I cried</p>"
+   :headers {}})
+
+(defn about
+  "Information about the website developer"
+  [request]
+  {:status 200
+   :body "I am an awesome Clojure developer, well getting there..."
+   :headers {}})
+
+(defn hello
+  "A simple personalised greeting showing the use of variable path elements"
+  [request]
+  (let [name (get-in request [:route-params :name])]
+    {:status 200
+     :body (str "Hello " name ".  I got your name from the web URL")
+     :headers {}}))
+
+(def operands {"+" + "-" - "*" * ":" /})
+
+(defn calculator
+  "A very simple calculator that can add, divide, subtract and multiply.  This is done through the magic of variable path elements."
+  [request]
+  (let [a  (Integer. (get-in request [:route-params :a]))
+        b  (Integer. (get-in request [:route-params :b]))
+        op (get-in request [:route-params :op])
+        f  (get operands op)]
+    (if f
+      {:status 200
+       :body (str (f a b))
+       :headers {}}
+      {:status 404
+       :body "Sorry, unknown operator.  I only recognise + - * : (: is for division)"
+       :headers {}})))
+
+(defroutes app
+           (GET "/" [] welcome)
+           (GET "/goodbye" [] goodbye)
+           (GET "/about" [] about)
+           (GET "/request-info" [] handle-dump)
+           (GET "/hello/:name" [] hello)
+           (GET "/calculator/:a/:op/:b" [] calculator)
+           (not-found "Sorry, page not found"))
+
+(defn -main
+  "A very simple web server using Ring & Jetty"
+  [port-number]
+  (jetty/run-jetty app
+                   {:port (Integer. port-number)}))
+
+(defn -dev-main
+  "A very simple web server using Ring & Jetty that reloads code changes via the development profile of Leiningen"
+  [port-number]
+  (jetty/run-jetty (wrap-reload #'app)
+                   {:port (Integer. port-number)}))
